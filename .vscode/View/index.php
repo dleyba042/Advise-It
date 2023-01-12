89b0f489bc7ade4f9e943553e5e59423bce88855
Devin Leyba <dleyba042@gmail.com>
2023-01-12 10:31:23 -0800

    "Initialize git repo with index page"

---
+++ .vscode/View/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Advise-It Tool</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <h1>Welcome to the Advise-It Tool</h1>
+    <button type="submit">Create a Plan?</button>
+    <script src="index.js"></script>
+    
+    <?php
+
+        echo "<h3> HEY Yo, php works!! </h3>"
+    ?>
+
+  </body>
+</html>

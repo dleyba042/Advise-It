b182bb3f9b0921d43f433bfb054cc64a6cad2181
Devin Leyba <dleyba042@gmail.com>
2023-01-12 10:56:39 -0800

    I am going to see if I can commit files I edited from server

---
+++ CapstoneProject/View/advise.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Advise-It Tool</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <h1>Welcome to the Advise-It Tool</h1>
+    <?php
+        echo "<h3> I will be sure to advise you now!!</h3>"
+    ?>
+    </form>
+
+  </body>
+</html>

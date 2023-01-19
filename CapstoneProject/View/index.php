aca2d4c6e936a8d88893c4ad8c882c5496adcbb0
Devin Leyba <dleyba042@gmail.com>
2023-01-19 12:55:35 -0800

    adds some js validation

--- CapstoneProject/View/index.php
+++ CapstoneProject/View/index.php
@@ -15,15 +15,14 @@
           <button id="plan_button" class= "index_button" type="submit">Create Plan</button>
         </form>   
         <button id="admin_button" class = "index_button" >Admin Login</button>
-        <div id = "entry_div">
+        <div id = "hidden_div" class="hidden">
           <label>Username: <input type = "text" id = "user_name"></label> 
+          <span class="hidden" id = "user_warning">Incorrect username entered</span>
           <label>Password: <input type = "text" id = "password"> </label>
+          <span class="hidden" id = "pass_warning">Incorrect password entered</span>
           <button class = "index_button" id ="login_button"> Login</button>
         </div>      
       </div>  
-    
-    
-      
     
       <script src='../JS/index.js' type='text/javascript'> </script>
   </body>

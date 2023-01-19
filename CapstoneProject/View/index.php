9696ca7492f0c592916ba3d6966b90f3245b18a6
Devin Leyba <dleyba042@gmail.com>
2023-01-18 17:40:19 -0800

    creates a log in button and styling

--- CapstoneProject/View/index.php
+++ CapstoneProject/View/index.php
@@ -11,7 +11,13 @@
     <form action="advise.php" method="get">
       <div id="index_div">
         <h1>Welcome to the Advise-It Tool</h1>
-        <button id="plan_button" type="submit">Create a Plan?</button>
+        <button id="plan_button" class= "index_button" type="submit">Create Plan</button>
+        <button id="admin_button" class = "index_button" >Admin Login</button>
+        <div id = "entry_div" class = "hide">
+          <label>Username: <input type = "text" id = "user_name"></label> 
+          <label>Password: <input type = "text" id = "password"> </label>
+          <button class = "index_button" form="admin_sbmt" action="submit"> Login</button>
+        </div>  
       </div>
     </form>
     <script src="index.js"></script>

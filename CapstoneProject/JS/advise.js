6e554a3a796c7dcaeb0307fbb66e84ee48217dd2
Devin Leyba <dleyba042@gmail.com>
2023-01-29 11:21:06 -0800

    makes js form addition work

--- CapstoneProject/JS/advise.js
+++ CapstoneProject/JS/advise.js
@@ -3,6 +3,7 @@ const printButton = document.getElementById("print_button");
 const prevButton = document.getElementById("prev_button");
 const nextButton = document.getElementById("next_button");
 const planDiv = document.getElementById("all_plans");
+const months = ["fall", "winter", "spring", "summer"];
 
 planForm.onsubmit = () => {
   alert("New information being saved in the database.");
@@ -22,25 +23,17 @@ $("#prev_button").click(function (e) {
   if (initialYear - nextYearUp > 2) {
     alert("cannot got back further than two years!");
   } else {
-    //Create an initial DB entry for this plan year
-    e.preventDefault();
-    $.ajax({
-      url: "https://dleyba-brown.greenriverdev.com/CapstoneProject/View/process.php",
-      type: "POST",
-      data: { token: studentToken, year: nextYearUp },
-      success: function () {
-        alert("ok");
-      },
-    });
-
     //Adjust previous button value
     prevButton.value = `${studentToken}:${nextYearUp - 1}:${initialYear}`;
 
-    //Append a new template for this year
+    //prepend a new template for this year
+    let form = createNewPlanForm(nextYearUp);
 
+    //Create header for new from
     let header = document.createElement("h1");
-    header.innerHTML = "YEAHHH";
+    header.innerHTML = nextYearUp + " School Year";
 
+    planDiv.prepend(form);
     planDiv.prepend(header);
   }
 });
@@ -54,30 +47,47 @@ $("#next_button").click(function (e) {
   if (nextYearUp - initialYear > 17) {
     alert("cannot go further than 2040!");
   } else {
-    //Create an initial DB entry for this plan year
-
-    console.log("Next BUTTON :: INIT YEAR: " + nextYearUp);
-
-    e.preventDefault();
-    $.ajax({
-      url: "https://dleyba-brown.greenriverdev.com/CapstoneProject/View/process.php",
-      type: "POST",
-      data: { token: studentToken, year: nextYearUp },
-      success: function () {
-        alert("ok");
-      },
-    });
-
     //Adjust next button value
     nextButton.value = `${studentToken}:${
       parseInt(nextYearUp) + 1
     }:${initialYear}`;
 
     //Append a new template for this year
+    let form = createNewPlanForm(nextYearUp);
 
+    //Create header for new from
     let header = document.createElement("h1");
-    header.innerHTML = "YEAHHH";
+    header.innerHTML = nextYearUp + " School Year";
 
     planDiv.append(header);
+    planDiv.append(form);
   }
 });
+
+const createNewPlanForm = (year) => {
+  let container = document.createElement("div");
+  container.classList.add("plan_container");
+
+  for (let i = 0; i < months.length; i++) {
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("textContainer");
+
+    let header = document.createElement("h5");
+    if (i == 0) {
+      header.innerHTML = `${months[i]} ${year}`;
+    } else {
+      header.innerHTML = `${months[i]} ${parseInt(year) + 1}`;
+    }
+
+    let textarea = document.createElement("textarea");
+
+    textarea.id = months[i];
+    textarea.name = `${months[i]}_${year}`;
+
+    textContainer.appendChild(header);
+    textContainer.appendChild(textarea);
+    container.appendChild(textContainer);
+  }
+
+  return container;
+};
